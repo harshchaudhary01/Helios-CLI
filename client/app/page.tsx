6ee117a94a1fbf1513c45ac7a27965b1f8f7ba08
harshchaudhary01 <harshcareer01@gmail.com>
2026-01-13 14:55:00 +0530

    Setting up the Client Directory

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Tooltip } from "@/components/ui/tooltip";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button>Click Me</Button>
+    </div>
+  );
+}
