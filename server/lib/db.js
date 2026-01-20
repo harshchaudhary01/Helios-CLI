705a9b96826466f4eca43cb21a1c9770f673cf0d
harshchaudhary01 <harshcareer01@gmail.com>
2026-01-20 21:11:41 +0530

    Implementing Dark Mode using Shadcn Theme-provider

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,8 +1,29 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const globalForPrisma = global
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis;
 
-if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const pool =
+  globalForPrisma.pgPool ??
+  new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.pgPool = pool;
+}
+
+const adapter = new PrismaPg(pool);
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
